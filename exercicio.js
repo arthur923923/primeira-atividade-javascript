3550cf64ee8db6c76887ca17cd477dcde98f1590
arthur923923 <renankk12320oiue@gmail.com>
2026-05-28 13:28:12 -0300

    primeira atividade java

---
+++ exercicio.js
@@ -0,0 +1,13 @@
+alert("bem vindo ao frances burguer")
+let lanche = prompt("qual lanche vc quer ?    " + " x-bacon " +" x-tudo " +
+    " x-ratão "
+) 
+let fritas = prompt(" acampanha fritas ?")
+
+let bebidas = prompt("qual bebida vc quer ?   " + " agua " +"coca "+ "cerveja")
+
+let entrega =prompt(" quer que entregue ? se sim coloque o endereço ")
+
+prompt("entao vc vai querer " + lanche +" fritas ? " + fritas + " sua bebida é " + bebidas + " e entrega " + entrega + "esta correto ?" ) 
+prompt(" credito ou debito ou pix ?") 
+alert("pedido feito obrigodo💓")
---
+++ script.js
@@ -0,0 +1,8 @@
+alert("bem vindo ao site da sua rua");
+let nomeUsuario = prompt("qual seu nome ?");
+let idade = prompt("qual a sua idade ?");
+let nomeRua = prompt("qual seu endereço ?");
+console.log(nomeUsuario)
+console.log(idade)
+console.log(nomeRua)
+alert("para confirmar seu nome é " + nomeUsuario + " e sua idade é" + idade + " e seu endereco é " + nomeRua);
